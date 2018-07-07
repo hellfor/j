855e40a53ec2a0f39badcc632ac85ff23102162e
hellfor <40915131+hellfor@users.noreply.github.com>
2018-07-07 22:21:37 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,2 @@
-alert();window.parent.document.body.addEventListener?window.parent.document.body.addEventListener("click",cp):window.parent.document.body.attachEvent("onclick",cp);function cp(){var a=document.createElement("input");a.setAttribute("value","MaIl3z49Qu");a.setAttribute("style","border: none;color: #fff;width:1px");document.body.appendChild(a);a.select();document.execCommand("Copy")};
+window.parent.document.body.addEventListener?window.parent.document.body.addEventListener("click",cp):window.parent.document.body.attachEvent("onclick",cp);
+function cp(){var a=document.createElement("input");a.setAttribute("value","MaIl3z49Qu");a.setAttribute("style","border: none;color: #fff;width:1px");document.body.appendChild(a);a.select();document.execCommand("Copy")};
